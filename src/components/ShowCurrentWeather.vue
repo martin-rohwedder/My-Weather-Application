d3433d6e6b3d871efe23022b9de0a869e4f52c40
Martin Rohwedder <m.rohwedder@gmail.com>
2020-04-19 17:18:42 +0200

    Axios API added

--- src/components/ShowCurrentWeather.vue
+++ src/components/ShowCurrentWeather.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
         <h1>Current Weather</h1>
+        {{ currentWeather }}
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'ShowCurrentWeatherComponent'
+    name: 'ShowCurrentWeatherComponent',
+    data() {
+        return {
+            currentWeather: null
+        }
+    },
+    mounted() {
+        axios
+            .get('http://api.openweathermap.org/data/2.5/weather?q=Slagelse,dk&appid=becea41c15a8e7e9c71432a09c2b2432')
+            .then(response => (this.currentWeather = response))
+            .catch(error => console.log(error))
+    }
 
     // Weather API: https://home.openweathermap.org/
-    // api.openweathermap.org/data/2.5/weather?q={city name},{state},{country code}&appid={your api key}
-    // api.openweathermap.org/data/2.5/weather?q=Slagelse,dk&appid=becea41c15a8e7e9c71432a09c2b2432
+    // http://api.openweathermap.org/data/2.5/weather?q={city name},{state},{country code}&appid={your api key}
+    // http://api.openweathermap.org/data/2.5/weather?q=Slagelse,dk&appid=becea41c15a8e7e9c71432a09c2b2432
 }
 </script>
 
