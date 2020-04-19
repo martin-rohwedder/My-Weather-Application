edca2b9ca5f1feb7dbfbd3c02aa4eb8647837b6d
Martin Rohwedder <m.rohwedder@gmail.com>
2020-04-19 17:48:32 +0200

    loading and error conditionals added

--- src/components/ShowCurrentWeather.vue
+++ src/components/ShowCurrentWeather.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        {{ currentWeather }}
+        <section v-if="errored">
+            <p>Sorry, but something happened when we tried to get the data. Please try again.</p>
+        </section>
+
+        <section v-else>
+            <div v-if="loading">
+                The data is loading ...
+            </div>
+
+            <div v-else>
+                {{ currentWeather }}
+            </div>
+        </section>
     </div>
 </template>
 
@@ -11,14 +23,20 @@ export default {
     name: 'ShowCurrentWeatherComponent',
     data() {
         return {
-            currentWeather: null
+            currentWeather: null,
+            loading: true,
+            errored: false
         }
     },
     mounted() {
         axios
             .get('http://api.openweathermap.org/data/2.5/weather?q=Slagelse,dk&appid=becea41c15a8e7e9c71432a09c2b2432')
             .then(response => (this.currentWeather = response.data))
-            .catch(error => console.log(error))
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false)
     }
 
     // Weather API: https://home.openweathermap.org/
