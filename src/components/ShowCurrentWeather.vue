b2d40c95b77a77b4ff0230cf93bc3f7714ba0ba8
Martin Rohwedder <m.rohwedder@gmail.com>
2020-04-19 19:26:18 +0200

    show JSON value data

--- src/components/ShowCurrentWeather.vue
+++ src/components/ShowCurrentWeather.vue
@@ -11,6 +11,14 @@
 
             <div v-else>
                 {{ currentWeather }}
+
+                <br />
+                <br />
+                
+                <div v-for="(value) in this.currentWeather.weather" v-bind:key="value.id">
+                    {{ value.description }}
+                </div>
+
             </div>
         </section>
     </div>
@@ -23,9 +31,9 @@ export default {
     name: 'ShowCurrentWeatherComponent',
     data() {
         return {
-            currentWeather: null,
+            currentWeather: {},
             loading: true,
-            errored: false
+            errored: false,
         }
     },
     mounted() {
@@ -36,7 +44,23 @@ export default {
                 console.log(error)
                 this.errored = true
             })
-            .finally(() => this.loading = false)
+            .finally(() => {
+                this.loading = false
+            })
+    },
+    methods: {
+        getCurrentWeather(cityName, country, language) {
+            axios
+                .get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',' + country + '&appid=becea41c15a8e7e9c71432a09c2b2432&lang=' + language)
+                .then(response => (this.currentWeather = response.data))
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => {
+                    this.loading = false
+                })
+        }
     }
 
     // Weather API: https://home.openweathermap.org/
