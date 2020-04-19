1d44845e434147ed89e42a5a53f4f102c55ae212
Martin Rohwedder <m.rohwedder@gmail.com>
2020-04-20 00:27:34 +0200

    SVG images shown

--- src/components/ShowCurrentWeather.vue
+++ src/components/ShowCurrentWeather.vue
@@ -10,15 +10,33 @@
             </div>
 
             <div v-else>
-                {{ currentWeather }}
-
-                <br />
-                <br />
-                
-                <div v-for="(value) in this.currentWeather.weather" v-bind:key="value.id">
-                    {{ value.description }}
+                <div id="weather-content">
+                    <div v-for="(value) in this.currentWeather.weather" v-bind:key="value.id">
+                        <div class="test">
+                            <div v-if="value.main === 'Clear'">
+                                <img v-bind:src="iconUrlClearDay" class="weather-icon-image" alt="">
+                            </div>
+                            <div v-else-if="value.main === 'Clouds'">
+                                <img v-bind:src="iconUrlCloud" alt="">
+                            </div>
+                            <div v-else-if="value.main === 'Rain'">
+                                <img v-bind:src="iconUrlRain" alt="">
+                            </div>
+                            <div v-else-if="value.main === 'Snow'">
+                                <img v-bind:src="iconUrlSnow" alt="">
+                            </div>
+                            <div v-else-if="value.main === 'Thunderstorm'">
+                                <img v-bind:src="iconUrlThunder" alt="">
+                            </div>
+                            <div v-else>
+                                <img v-bind:src="iconUrlCloud" alt="">
+                            </div>
+                        </div>
+                        <div class="test2">
+                            {{ value.description }}
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </section>
     </div>
@@ -34,6 +52,15 @@ export default {
             currentWeather: {},
             loading: true,
             errored: false,
+            iconUrlClearDay: require('../assets/weather-icons/day.svg'),
+            iconUrlClearNight: require('../assets/weather-icons/night.svg'),
+            iconUrlCloudDay: require('../assets/weather-icons/cloudy-day-2.svg'),
+            iconUrlCloudNight: require('../assets/weather-icons/cloudy-night-2.svg'),
+            iconUrlCloud: require('../assets/weather-icons/cloudy.svg'),
+            iconUrlRainAndSun: require('../assets/weather-icons/rainy-3.svg'),
+            iconUrlRain: require('../assets/weather-icons/rainy-5.svg'),
+            iconUrlSnow: require('../assets/weather-icons/snowy-6.svg'),
+            iconUrlThunder: require('../assets/weather-icons/thunder.svg'),
         }
     },
     mounted() {
@@ -70,5 +97,22 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+#weather-content
+    display flex
+    flex-direction column
+    align-items center
+
+.test
+    display flex
+    flex 1
+    width 50vw
+    align-items center
+
+.test2
+    display flex
+    flex 1
+
+.weather-icon-image
+    width: 350px
 
 </style>
